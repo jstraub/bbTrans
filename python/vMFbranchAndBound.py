1563bf6ee20f62904ebb605e59752d9d5966d63b
Julian Straub <jstraub@csail.mit.edu>
2015-09-15 21:27:46 -0400

    upper and lower bound eval

--- python/vMFbranchAndBound.py
+++ python/vMFbranchAndBound.py
@@ -36,6 +36,7 @@ def ComputevMFtovMFcost(R, vMFMM_A, vMFMM_B, j, k):
   C *= (np.exp(z_jk) - np.exp(-z_jk)) / z_jk
   return C
 
+
 def LowerBound(vMFMM_A, vMFMM_B, vertices, tetra):
   ''' 
   Compute a lowerbound on the objective by evaluating it at the center
@@ -63,16 +64,38 @@ def UpperBoundConvexity(vMFMM_A, vMFMM_B, vertices, tetra):
           vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
   return ub
 
+def near(a, b):
+  return np.abs(a-b) < 1e-6
+
+def ComputeClosestLocationOnGeodesic(mu1, mu2, nu):
+#  print mu1, mu2, nu
+  theta12 = np.arccos(mu1.dot(mu2))
+  theta1 = np.arccos(mu1.dot(nu))
+  theta2 = np.arccos(mu2.dot(nu))
+  if near(theta1, np.pi*0.5) and near(theta2, np.pi*0.5):
+    # any t is good.
+    t = 0.5
+  t = (np.arctan2(np.cos(theta2) - np.cos(theta12)*np.cos(theta1),np.cos(theta1)*np.sin(theta12))) / theta12
+  return (mu1*np.sin((1.-t)*theta12) + mu2*np.sin(t*theta12))/np.sin(theta12)
 
 def ClosestMu(muA, muB, qs):
-  mus = np.array([q.rotate(muB) for q in qs])
-  a = np.linalg.solve(mus, muA)
-  if np.all(a > 0.):
-    return muA
-  else:
-    #TODO
+  mus = np.array([q.toRot().R.dot(muB) for q in qs]).T
+#  print mus.shape, muA.shape
+#  print mus.T.dot(mus), mus.T.dot(muA)
+#  a = np.linalg.solve(mus.T.dot(mus), mus.T.dot(muA))
+#  print a
+#  if np.all(a > 0.):
+#    return muA
+#  else:
+  mus = mus.T
+  closestLocations = []
+  for i, mu1 in enumerate(mus):
+    for mu2 in mus[i+1::]:
+      closestLocations.append(ComputeClosestLocationOnGeodesic(mu1,
+        mu2, muA)) 
+  dists = np.array([mu.dot(muA) for mu in closestLocations])
+  return closestLocations[np.argmax(dists)]
    
-
 def UpperBound(vMFMM_A, vMFMM_B, vertices, tetra):
   ''' 
   '''
@@ -81,6 +104,20 @@ def UpperBound(vMFMM_A, vMFMM_B, vertices, tetra):
   for j in range(vMFMM_A.GetK()):
     for k in range(vMFMM_B.GetK()):
       mu_star = ClosestMu(vMFMM_A.GetvMF(j).GetMu(), vMFMM_B.GetvMF(k).GetMu(), qs)
+      C = 2. * np.pi * vMFMM_A.GetPi(j) * vMFMM_B.GetPi(k) * \
+          vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
+      z_jk = np.sqrt(((vMFMM_A.GetvMF(j).GetTau() *
+        vMFMM_A.GetvMF(j).GetMu() + vMFMM_A.GetvMF(k).GetTau() *
+        mu_star)**2).sum())
+      C *= (np.exp(z_jk) - np.exp(-z_jk)) / z_jk
+      ub += C
+#      M = mu_star.dot(vMFMM_B.GetvMF(j).GetMu())
+#      z = np.sqrt(vMFMM_B.GetvMF(j).GetTau()**2 +
+#          vMFMM_B.GetvMF(k).GetTau()**2 + 2.*vMFMM_B.GetvMF(j).GetTau()
+#         *vMFMM_B.GetvMF(k).GetTau()*M)
+#      D = 2. * np.pi * vMFMM_A.GetPi(j) * vMFMM_B.GetPi(k) * \
+#          vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
+#      ub += D*(np.exp(z) - np.exp(-z))/z
   return ub
 
 if __name__ == "__main__":
@@ -88,20 +125,25 @@ if __name__ == "__main__":
   print s3.tetra_levels
 
 
-  vMFs_A = [vMF(np.array([1.,0.,0.]), 1.)]
-  vMFs_B = [vMF(np.array([1.,0.,0.]), 1.)]
-  vMFMM_A = vMFMM(np.array([1.]), vMFs_A)
-  vMFMM_B = vMFMM(np.array([1.]), vMFs_B)
+  vMFs_A = [vMF(np.array([1.,0.,0.]), 1.), vMF(np.array([0.,1.,0.]), 10.)]
+  vMFs_B = [vMF(np.array([1.,0.,0.]), 1.), vMF(np.array([0.,0.,1.]), 10.)]
+  vMFMM_A = vMFMM(np.array([0.5, 0.5]), vMFs_A)
+  vMFMM_B = vMFMM(np.array([0.5, 0.5]), vMFs_B)
 
   tetras = s3.GetTetras(0)
   print tetras.shape
   lb = np.zeros((tetras.shape[0], 1))
+  ub = np.zeros((tetras.shape[0], 1))
   for i in range(s3.tetra_levels[-1]):
     lb[i] = LowerBound(vMFMM_A, vMFMM_B, s3.vertices, tetras[i,:])
+    ub[i] = UpperBound(vMFMM_A, vMFMM_B, s3.vertices, tetras[i,:])
   print lb.T
+  print ub.T
+  print np.all(ub > lb)
+  print np.sum(ub > lb)
 
-  print np.argmax(lb)
-  print np.max(lb)
+  print np.argmax(lb), np.argmax(ub)
+  print np.max(lb), np.max(ub)
   
   vertices = s3.vertices
   tetraMax = tetras[np.argmax(lb),:] 
