50d27fd9beed23d7826dfbdfc5bde398679adaf0
Julian Straub <jstraub@csail.mit.edu>
2015-09-16 09:50:28 -0400

    pulled out vMF and vMFMM classes

--- python/testvMFBBbounds.py
+++ python/testvMFBBbounds.py
@@ -2,41 +2,11 @@ import numpy as np
 from js.geometry.rotations import *
 from js.geometry.sphere import Sphere
 from discretized4dSphere import S3Grid
+from vMFMM import *
 import mayavi.mlab as mlab
 
-class vMF(object):
-  def __init__(self, mu, tau):
-    self.mu = np.copy(mu)
-    self.tau = tau
-    self.Z = self.ComputePartitionFunction()
-  def GetTau(self):
-    return self.tau
-  def GetMu(self):
-    return self.mu
-  def GetZ(self):
-    return self.Z
-  def ComputePartitionFunction(self):
-    return self.tau / (2.*np.pi*(np.exp(self.tau) - np.exp(-self.tau)))
-
-class vMFMM(object):
-  def __init__(self, pis, vMFs):
-    self.pis = pis / pis.sum()
-    self.vMFs = vMFs
-  def GetvMF(self, k):
-    return  self.vMFs[k]
-  def GetPi(self, k):
-    return  self.pis[k]
-  def GetK(self):
-    return self.pis.size
-
-def ComputevMFtovMFcost(vMFMM_A, vMFMM_B, j, k, nu):
-  C = 2. * np.pi * vMFMM_A.GetPi(j) * vMFMM_B.GetPi(k) * \
-      vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
-  z_jk = np.sqrt(((vMFMM_A.GetvMF(j).GetTau() *
-    vMFMM_A.GetvMF(j).GetMu() + vMFMM_B.GetvMF(k).GetTau() *
-    nu)**2).sum())
-  C *= (np.exp(z_jk) - np.exp(-z_jk)) / z_jk
-  return C
+def near(a, b):
+  return np.abs(a-b) < 1e-6
 
 def LowerBound(vMFMM_A, vMFMM_B, vertices, tetra):
   ''' 
@@ -64,9 +34,6 @@ def UpperBoundConvexity(vMFMM_A, vMFMM_B, vertices, tetra):
       ub += 2. * np.pi * vMFMM_A.GetPi(j) * vMFMM_B.GetPi(k) * \
           vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
   return ub
-
-def near(a, b):
-  return np.abs(a-b) < 1e-6
 
 def ComputeClosestLocationOnGeodesic(mu1, mu2, nu):
 #  print mu1, mu2, nu
@@ -130,7 +97,6 @@ def UpperBound(vMFMM_A, vMFMM_B, vertices, tetra):
 if __name__ == "__main__":
   s3 = S3Grid(0)
   print s3.tetra_levels
-
 
   vMFs_A = [vMF(np.array([1.,0.,0.]), 1.), vMF(np.array([0.,1.,0.]), 10.)]
   vMFs_B = [vMF(np.array([1.,0.,0.]), 1.), vMF(np.array([0.,0.,1.]), 10.)]
---
+++ python/vMFMM.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class vMF(object):
+  def __init__(self, mu, tau):
+    self.mu = np.copy(mu)
+    self.tau = tau
+    self.Z = self.ComputePartitionFunction()
+  def GetTau(self):
+    return self.tau
+  def GetMu(self):
+    return self.mu
+  def GetZ(self):
+    return self.Z
+  def ComputePartitionFunction(self):
+    return self.tau / (2.*np.pi*(np.exp(self.tau) - np.exp(-self.tau)))
+
+class vMFMM(object):
+  def __init__(self, pis, vMFs):
+    self.pis = pis / pis.sum()
+    self.vMFs = vMFs
+  def GetvMF(self, k):
+    return  self.vMFs[k]
+  def GetPi(self, k):
+    return  self.pis[k]
+  def GetK(self):
+    return self.pis.size
+
+def ComputevMFtovMFcost(vMFMM_A, vMFMM_B, j, k, nu):
+  C = 2. * np.pi * vMFMM_A.GetPi(j) * vMFMM_B.GetPi(k) * \
+      vMFMM_A.GetvMF(j).GetZ() * vMFMM_A.GetvMF(k).GetZ() 
+  z_jk = np.sqrt(((vMFMM_A.GetvMF(j).GetTau() *
+    vMFMM_A.GetvMF(j).GetMu() + vMFMM_B.GetvMF(k).GetTau() *
+    nu)**2).sum())
+  C *= (np.exp(z_jk) - np.exp(-z_jk)) / z_jk
+  return C
