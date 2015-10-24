3a22ee1969735a37b9d8ef7d6fa891687c9b581c
Julian Straub <jstraub@csail.mit.edu>
2015-10-24 08:04:17 -0400

    fixed GMM T computation

--- test/branch_and_bound_R3.cpp
+++ test/branch_and_bound_R3.cpp
@@ -16,16 +16,17 @@ int main(int argc, char** argv) {
 
   Eigen::Vector3d t_true(1.,1.,1.);
   std::cout << "true translation: " << t_true.transpose() << std::endl;
-  Eigen::Quaterniond q;
-  q.setIdentity();
-  //std::cout << q_true.toRotationMatrix() << std::endl;
+  Eigen::Quaterniond q(1.,1.,1.,1.);
+  q.normalize();
+  Eigen::Matrix3d R = q.toRotationMatrix();
+  std::cout << R << std::endl;
   
   Eigen::Vector3d muA1, muA2;
   muA1 << 1.,0.,0.;
   muA2 << 0.,1.,0.;
   Eigen::Vector3d muB1, muB2;
-  muB1 = muA1 + t_true;
-  muB2 = muA2 + t_true;
+  muB1 = R*muA1 + t_true;
+  muB2 = R*muA2 + t_true;
 
   Eigen::Matrix3d Sigma1 = Eigen::Matrix3d::Identity();
   Eigen::Matrix3d Sigma2 = Eigen::Matrix3d::Identity();
@@ -37,8 +38,18 @@ int main(int argc, char** argv) {
   gmmA.push_back(Normal<3>(muA2,Sigma2,0.7));
 
   std::vector<Normal<3>> gmmB;
-  gmmB.push_back(Normal<3>(muB1,Sigma1,0.3));
-  gmmB.push_back(Normal<3>(muB2,Sigma2,0.7));
+  gmmB.push_back(Normal<3>(muB1,R*Sigma1*R.transpose(),0.3));
+  gmmB.push_back(Normal<3>(muB2,R*Sigma2*R.transpose(),0.7));
+
+  std::vector<Normal<3>> gmmT;
+  ComputeGmmT(gmmA, gmmB, gmmT, q);
+
+  std::cout << "GMM A" << std::endl;
+  for (auto& g: gmmA) g.Print();
+  std::cout << "GMM B" << std::endl;
+  for (auto& g: gmmB) g.Print();
+  std::cout << "GMM T" << std::endl;
+  for (auto& g: gmmT) g.Print();
   
   Eigen::Vector3d min, max;
   min << 0.,0.,0.;
